b809c49e36d4fac1d95ef0471541bb33f9516499
Hamim <hamimsiddiq915@gmail.com>
2026-06-18 22:48:43 +0600

    video model pblm fixed

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -4,7 +4,7 @@ import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const videoSchema = new Schema({
     videoFile: {
         type: String,
-        required: [true, "Video file is required"],,
+        required: [true, "Video file is required"],
     },
     thumbnail: {
         type: String,
