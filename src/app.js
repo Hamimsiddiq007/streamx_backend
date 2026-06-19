07f392ebeb692989e755e8d54a5923fb053d938a
Hamim <hamimsiddiq915@gmail.com>
2026-06-19 22:59:44 +0600

    feat: register user

--- src/app.js
+++ src/app.js
@@ -14,4 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
+// Routes
+import userRoutes from './routes/user.route.js';
+
+app.use('/api/v1/users', userRoutes);
+
 export default app;
