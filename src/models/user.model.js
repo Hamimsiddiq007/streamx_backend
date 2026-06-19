c7547ea546f3029b6a03534268d24052bfb6330a
Hamim <hamimsiddiq915@gmail.com>
2026-06-19 12:42:57 +0600

    password encription

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,8 +50,13 @@ const userSchema = new Schema(
 );
 
 userSchema.pre("save", async function (next) {
+  if(!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+}
 
 export const User = mongoose.model("User", userSchema);
