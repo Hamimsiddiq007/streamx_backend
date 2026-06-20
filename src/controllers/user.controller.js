70ce1f00a19e61657fc259c3c1cfd2fd082d9003
Hamim <hamimsiddiq915@gmail.com>
2026-06-20 15:17:15 +0600

    User created

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import asyncHandler from '../utils/asyncHandler.js';
 import ApiError from '../utils/ApiError.js';
 import { User } from '../models/user.model.js';
 import uploadOnCloudinary from '../utils/cloudinary.js';
+import ApiResponse from '../utils/ApiResponse.js';
 
 const registerUser = asyncHandler(async (req, res) => {
     // Get user data from frontend
@@ -43,6 +44,25 @@ const registerUser = asyncHandler(async (req, res) => {
     if(!avatar){
         throw new ApiError(400, "avatar upload failed");
     }
+
+    const user = await User.create({
+        fullname,
+        username : username.toLowerCase(),
+        email,
+        password,
+        avatar: avatar.secure_url,
+        coverImage: coverImage?.secure_url || ''
+    })
+
+    const createdUser = await User.findById(user._id).select('-password -refreshToken');
+
+    if(!createdUser){
+        throw new ApiError(500, "Something went wrong while registering user");
+    }
+    
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully")
+    )
 
 });
 
