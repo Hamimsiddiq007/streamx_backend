ecc7518712939514e1e99c224d952f152342fa27
Hamim <hamimsiddiq915@gmail.com>
2026-06-20 14:36:53 +0600

    multer middleware for image

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,18 @@
 import asyncHandler from '../utils/asyncHandler.js';
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'User registered successfully'});
+    // Get user data from frontend
+    // Validation - not emoty
+    // Check if user already exists : email, username
+    // Check for image, check for avatar
+    // upload them to cloudinary
+    // Create user object - entry in database
+    // remove password from response
+    // send response to frontend
+
+    const {username, email, password, fullname} = req.body;
+    console.log('email :', email);
 });
+
 
 export {registerUser};
