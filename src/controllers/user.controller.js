e5ce70c2c5451442fb99ea3b8b2f1461a35afbda
Hamim <hamimsiddiq915@gmail.com>
2026-06-22 22:31:02 +0600

    refreshAccessToken route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -191,4 +191,4 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 });
 
 
-export {registerUser, loginUser, logoutUser};
+export {registerUser, loginUser, logoutUser, refreshAccessToken};
