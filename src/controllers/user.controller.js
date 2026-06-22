cf757c887f6b0cfb0966caf46f9997fb850ad88a
Hamim <hamimsiddiq915@gmail.com>
2026-06-22 20:15:20 +0600

    feat: logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -127,6 +127,21 @@ const loginUser = asyncHandler(async (req, res) => {
 const logoutUser = asyncHandler(async (req, res) => {
     // remove tokens from cookies
     // send response
+    User.findOneAndUpdate(
+        {_id: req.user._id},
+        {refreshToken: undefined},
+        {new: true}
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none"
+    }
+
+    return res.status(200).clearCookie("accessToken", options).clearCookie("refreshToken", options).json(
+        new ApiResponse(200, {}, "User logged out successfully")
+    )
 
 });
 
