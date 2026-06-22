c0c4e00ef47f75cb4939c78305e08ae7a733a271
Hamim <hamimsiddiq915@gmail.com>
2026-06-22 19:18:10 +0600

    generate access & refresh token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,22 @@ import { User } from '../models/user.model.js';
 import uploadOnCloudinary from '../utils/cloudinary.js';
 import ApiResponse from '../utils/ApiResponse.js';
 
+const generateAccessAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken };
+
+    } catch (error) {
+        throw new ApiError(500, "Error generating tokens");
+    }
+}
+
 const registerUser = asyncHandler(async (req, res) => {
     // Get user data from frontend
     // Validation - not emoty
@@ -86,6 +102,14 @@ const loginUser = asyncHandler(async (req, res) => {
     if(!user){
         throw new ApiError(404, "User does not exist");
     }
+
+    const isPasswordValid = await user.comparePassword(password);
+
+    if(!isPasswordValid){
+        throw new ApiError(401, "Password is incorrect");
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id);
 });
 
 
